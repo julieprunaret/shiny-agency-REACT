4d76937af63a63894687d8aa6ca00dfa3467734f
Julie <j.prunaret@gmail.com>
2023-10-16 08:28:46 +0200

    add router logic

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+function Card({ label, title, picture }) {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', padding: 15 }}>
+      <span>{label}</span>
+      <img src={picture} alt="freelance" height={80} width={80} />
+      <span>{title}</span>
+    </div>
+  );
+}
+Card.defaultProps = {
+  title: 'Mon titre par défaut',
+};
+Card.propTypes = {
+  label: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  picture: PropTypes.string,
+};
+
+export default Card;
